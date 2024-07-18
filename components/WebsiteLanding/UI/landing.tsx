d6594a6162b117bf3590800b084b89adbf9ed33a
Risav03 <needleweed03@gmail.com>
2024-07-19 01:04:52 +0530

    go

--- components/WebsiteLanding/UI/landing.tsx
+++ components/WebsiteLanding/UI/landing.tsx
@@ -4,7 +4,7 @@ import landingBg from "@/assets/WebsiteLanding/fillers/bgLanding.png"
 
 export const Landing = () => {
   return (
-    <div className='flex items-end min-h-screen max-md:object-cover max-md:overflow-hidden' >
+    <div className='flex items-end min-h-screen max-md:object-cover max-md:overflow-hidden md:mt-40' >
         <Image src={landingBg} alt='bg1' className='w-screen'/>
     </div>
   )
