95eb0525a0d1e6310bb0ec6cce56cfb8355affc9
Risav03 <needleweed03@gmail.com>
2024-07-19 22:40:56 +0530

    gif add

--- components/WebsiteLanding/UI/landing.tsx
+++ components/WebsiteLanding/UI/landing.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
-import landingBg from "@/assets/WebsiteLanding/fillers/bgLanding.png"
+import landingBg from "@/assets/WebsiteLanding/fillers/bgLanding.gif"
 
 export const Landing = () => {
   return (
-    <div className='flex items-end min-h-screen max-md:object-cover max-md:overflow-hidden max-sm:w-[32rem] max-md:w-[50rem] md:mt-40' >
+    <div className='flex items-end min-h-screen max-md:object-cover max-md:overflow-hidden max-sm:w-[32rem] max-md:w-[50rem]' >
         <Image src={landingBg} alt='bg1' className='max-sm:w-[32rem] max-md:w-[50rem]'/>
     </div>
   )
