80f232423e388175242814ea58ed5f0a76a4abed
Risav03 <needleweed03@gmail.com>
2024-07-20 01:10:34 +0530

    go

--- components/WebsiteLanding/UI/footComp.tsx
+++ components/WebsiteLanding/UI/footComp.tsx
@@ -4,8 +4,13 @@ import Image from 'next/image'
 
 export const FootComp = () => {
   return (
-    <div className='w-full h-[20rem] bg-[#1E1E1E] brightness-90 overflow-hidden' >
-        {/* <Image src={footerbg} alt='footerbg' /> */}
+    <div className='w-full h-[20rem] bg-[#1E1E1E] p-10 brightness-90 overflow-hidden flex flex-row' >
+      <div>
+        <h2 className='text-6xl text-white' >CHUM CHUMS</h2>
+        <div className='grid grid-flow-col grid-cols-2'>
+
+        </div>
+      </div>
     </div>
   )
 }
