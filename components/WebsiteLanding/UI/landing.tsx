dac3d2ae8475f37ccc6f8a10b8e22da8252d66ab
Risav03 <needleweed03@gmail.com>
2024-07-19 12:16:24 +0530

    landing pic adjust

--- components/WebsiteLanding/UI/landing.tsx
+++ components/WebsiteLanding/UI/landing.tsx
@@ -4,8 +4,8 @@ import landingBg from "@/assets/WebsiteLanding/fillers/bgLanding.png"
 
 export const Landing = () => {
   return (
-    <div className='flex items-end min-h-screen max-md:object-cover max-md:overflow-hidden max-md:w-[32rem] md:mt-40' >
-        <Image src={landingBg} alt='bg1' className='max-md:w-[32rem]'/>
+    <div className='flex items-end min-h-screen max-md:object-cover max-md:overflow-hidden max-sm:w-[32rem] max-md:w-[50rem] md:mt-40' >
+        <Image src={landingBg} alt='bg1' className='max-sm:w-[32rem] max-md:w-[50rem]'/>
     </div>
   )
 }
