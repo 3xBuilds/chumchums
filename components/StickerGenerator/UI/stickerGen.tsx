53288db053f5eaf3758a2080ac5737c7fd402a29
Risav03 <needleweed03@gmail.com>
2024-07-20 22:52:37 +0530

    push

--- components/StickerGenerator/UI/stickerGen.tsx
+++ components/StickerGenerator/UI/stickerGen.tsx
@@ -30,6 +30,14 @@ export const StickerGen:React.FC = () => {
     const amount = [1, 76, 71, 23, 87, 14];
 
     const handleDownload = async () => {
+
+        //@ts-ignore
+        html2canvas(document.getElementById("capture")).then(canvas => {
+            console.log(canvas.width, canvas)
+            // document.body.appendChild(canvas)
+        });
+
+
         if (!layeredImageRef.current) return;
       
         const parentDiv = layeredImageRef.current.parentElement;
@@ -41,14 +49,11 @@ export const StickerGen:React.FC = () => {
         // Calculate the height to maintain aspect ratio
         const aspectRatio = layeredImageRef.current.offsetWidth / layeredImageRef.current.offsetHeight;
         const height = Math.round(parentWidth / aspectRatio);
-      
-        const canvas = await html2canvas(layeredImageRef.current, {
-          width: 1000,
-          height: 1000,
-          scale: 1, // Adjust this value for higher resolution if needed
-          useCORS: true,
-          logging: true,
-        });
+        
+        console.log(parentWidth, aspectRatio, height)
+
+        // @ts-ignore
+        const canvas = await html2canvas(document.getElementById("capture"));
       
         const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
         
@@ -61,7 +66,7 @@ export const StickerGen:React.FC = () => {
     useEffect(()=>{
         setDisplayArr([])
         const arr = Array.from({ length: amount[selectedType] }, (_, index) => index);
-        console.log(arr);
+        // console.log(arr);
         setDisplayArr(arr);
     },[selectedType])
       
@@ -80,47 +85,55 @@ export const StickerGen:React.FC = () => {
                     </button>
                 ))}
             </div>
-            <div className=' h-full gap-4 p-4 items-start max-md:justify-start md:justify-center flex md:flex-wrap max-md:w-fit overflow-x-scroll mx-auto' >
-                {displayArr?.length != 0 && displayArr.map((item:number)=>(
-                    <button onClick={()=>{if(selectedType==0)setSelectedTrait0(item); if(selectedType==1)setSelectedTrait1(item);if(selectedType==2)setSelectedTrait2(item);if(selectedType==3)setSelectedTrait3(item);if(selectedType==4)setSelectedTrait4(item);if(selectedType==5)setSelectedTrait5(item);}} className='flex cursor-pointer items-center justify-center w-40 h-40 bg-white/10 rounded-xl gap-5' >
-                        <Image className='rounded-xl' src={ selectedType==5 && (item==12 || item == 13) ? require(`../../../assets/StickerGenerator/trait_layers/${selectedType}/${item+1}.gif`): require(`../../../assets/StickerGenerator/trait_layers/${selectedType}/${item+1}.png`)} alt={String(item)} />
-                    </button>
-                ))}
+            <div className='overflow-x-scroll w-full max-md:h-40 h-full'>
+                <div className=' h-full gap-4 p-4 items-start max-md:justify-start md:justify-center md:flex md:flex-wrap max-md:w-fit max-md:grid max-md:grid-flow-col max-md:grid-rows-1 mx-auto' >
+                    {displayArr?.length != 0 && displayArr.map((item:number)=>(
+                        <button onClick={()=>{if(selectedType==0)setSelectedTrait0(item); if(selectedType==1)setSelectedTrait1(item);if(selectedType==2)setSelectedTrait2(item);if(selectedType==3)setSelectedTrait3(item);if(selectedType==4)setSelectedTrait4(item);if(selectedType==5)setSelectedTrait5(item);}} className='flex cursor-pointer items-center justify-center w-40 h-40 bg-white/10 rounded-xl gap-5' >
+                            <Image className='rounded-xl' src={ selectedType==5 && (item==12 || item == 13) ? require(`../../../assets/StickerGenerator/trait_layers/${selectedType}/${item+1}.gif`): require(`../../../assets/StickerGenerator/trait_layers/${selectedType}/${item+1}.png`)} alt={String(item)} />
+                        </button>
+                    ))}
+                </div>
             </div>
         </div>
         <div className='md:h-full md:w-[67%] h-[67%] w-full items-center flex flex-col mt-5 gap-5 justify-start'>
             <h2 className='text-3xl'>Sticker Generator</h2>
-            <div ref={layeredImageRef} className='max-md:w-[19rem] relative max-md:h-[19rem] overflow-hidden md:w-[32rem] md:h-[32rem] border-[5px] rounded-xl border-dashed border-white/15'>
+            <div id='capture' ref={layeredImageRef} className='max-md:w-[19rem] relative max-md:h-[19rem] overflow-hidden md:w-[32rem] md:h-[32rem] border-[5px] rounded-xl border-dashed border-white/15'>
                 
                     <Image 
                     src={(selectedTrait5==12 || selectedTrait5 == 13) ? require(`../../../assets/StickerGenerator/trait_layers/5/${selectedTrait5+1}.gif`): require(`../../../assets/StickerGenerator/trait_layers/5/${selectedTrait5+1}.png`)}
                     alt="Background" 
                     className='w-full absolute z-0'
+                    style={{width:"100%", height:"100%"}}
                     />
                     <Image 
                     src={require(`../../../assets/StickerGenerator/trait_layers/0/1.png`)}
                     alt="Outline" 
                     className='w-screen absolute z-1'
+                    style={{width:"100%", height:"100%"}}
                     />
                     <Image 
                     src={require(`../../../assets/StickerGenerator/trait_layers/1/${selectedTrait1+1}.png`)} 
                     alt="Head" 
                     className='w-full absolute z-2' 
+                    style={{width:"100%", height:"100%"}}
                     />
                     <Image 
                     src={require(`../../../assets/StickerGenerator/trait_layers/2/${selectedTrait2+1}.png`)} 
                     alt="Body" 
-                    className='w-full absolute z-2' 
+                    className='w-full absolute z-3' 
+                    style={{width:"100%", height:"100%"}}
                     />
                     <Image 
                     src={require(`../../../assets/StickerGenerator/trait_layers/3/${selectedTrait3+1}.png`)} 
                     alt="Face" 
-                    className='w-full absolute z-2' 
+                    className='w-full absolute z-4'
+                    style={{width:"100%", height:"100%"}} 
                     />
                     <Image 
                     src={require(`../../../assets/StickerGenerator/trait_layers/4/${selectedTrait4+1}.png`)} 
                     alt="Chum" 
-                    className='w-full absolute z-2' 
+                    className='w-full absolute z-5' 
+                    style={{width:"100%", height:"100%"}}
                     />
 
             </div>
