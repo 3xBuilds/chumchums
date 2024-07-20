7f144331e095e466894a94e84a685245caf61a17
Risav03 <needleweed03@gmail.com>
2024-07-20 11:03:21 +0530

    main page footer

--- components/WebsiteLanding/UI/footComp.tsx
+++ components/WebsiteLanding/UI/footComp.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
 // import footerbg from "@/assets/WebsiteLanding/footer/footerbg.jpg"
 import Image from 'next/image'
+import meow from "@/assets/WebsiteLanding/footer/meow.png"
 
 export const FootComp = () => {
   return (
-    <div className='w-full h-[20rem] bg-[#1E1E1E] p-10 brightness-90 overflow-hidden flex flex-row' >
-      <div>
-        <h2 className='text-6xl text-white' >CHUM CHUMS</h2>
-        <div className='grid grid-flow-col grid-cols-2'>
+    <div className='w-full md:h-[20rem] bg-[#1E1E1E] p-10 brightness-90 overflow-hidden items-center justify-center flex md:flex-row flex-col' >
+      <div className='md:w-[35%]' >
+        <h2 className='text-6xl max-md:text-4xl text-white' >CHUM CHUMS</h2>
+        <div className='md:grid md:grid-flow-col md:grid-cols-2 gap-10 mt-3'>
+            <div className='flex flex-col gap-2'>
+                <h3 className='text-[#CCFF00] text-[1.6rem]' >Contact Us</h3>
+                <a className='text-[1.1rem] text-white/90 hover:text-white duration-200 hover:brightness-125' target='_blank' href='mailto:chumchums@gmail.com' >chumchums@gmail.com</a>
+                <a href='https://www.tiktok.com/@chumchumsofficial' target='_blank' className='text-[1.1rem] text-white/90 hover:text-white duration-200 hover:brightness-125' >Instagram</a>
+                <a href='https://x.com/chumchumsnft' target='_blank' className='text-[1.1rem] text-white/90 hover:text-white duration-200 hover:brightness-125' >Twitter</a>
+                <a href='https://discord.gg/vVSkDyvCRC' target='_blank' className='text-[1.1rem] text-white/90 hover:text-white duration-200 hover:brightness-125' >Discord</a>
 
+
+            </div>
+            <div className='flex flex-col items-start gap-2 max-md:mt-5'>
+                <h3 className='text-[#CCFF00] text-[1.6rem]'>Links</h3>
+                <button className='text-[1.1rem] text-white/90 hover:text-white duration-200 hover:brightness-125'>Sticker Generator</button>
+                <a target='_blank' href='https://opensea.io/collection/chumchumsnft' className='text-[1.1rem] text-white/90 hover:text-white duration-200 hover:brightness-125' >OpenSea</a>
+            </div>
         </div>
+      </div>
+
+      <div className='h-full flex items-end md:w-[36%] justify-center my-10'>
+        <h6>All Rights Reserved by ChumChums ©</h6>
+      </div>
+
+      <div className='flex items-center justify-end md:w-[30%]' >
+          <Image src={meow} alt='meow' />
       </div>
     </div>
   )
