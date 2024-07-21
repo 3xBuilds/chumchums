495ce4ff48f07decac8bbb9cac980996e24cb647
Risav03 <needleweed03@gmail.com>
2024-07-21 20:58:40 +0530

    go

--- components/StickerGenerator/UI/stickerGen.tsx
+++ components/StickerGenerator/UI/stickerGen.tsx
@@ -30,13 +30,6 @@ export const StickerGen:React.FC = () => {
 
     const handleDownload = async () => {
 
-        //@ts-ignore
-        html2canvas(document.getElementById("capture")).then(canvas => {
-            console.log(canvas.width, canvas)
-            // document.body.appendChild(canvas)
-        });
-
-
         if (!layeredImageRef.current) return;
       
         const parentDiv = layeredImageRef.current.parentElement;
@@ -52,7 +45,11 @@ export const StickerGen:React.FC = () => {
         console.log(parentWidth, aspectRatio, height)
 
         // @ts-ignore
-        const canvas = await html2canvas(document.getElementById("capture"));
+        const canvas = await html2canvas(document.getElementById("capture"),{
+            useCORS: true,
+            allowTaint: true,
+            foreignObjectRendering: true,
+          });
       
         if(selectedTrait5 != 12 && selectedTrait5 != 13){
             const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
@@ -62,6 +59,7 @@ export const StickerGen:React.FC = () => {
             link.click();
         }
       };
+
 
     useEffect(()=>{
         setDisplayArr([])
@@ -76,7 +74,6 @@ export const StickerGen:React.FC = () => {
         setSelectedTrait3(Math.floor(amount[3]*Math.random()));
         setSelectedTrait4(Math.floor(amount[4]*Math.random()));
         setSelectedTrait5(Math.floor(amount[5]*Math.random()));
-
     }
       
 
@@ -98,7 +95,7 @@ export const StickerGen:React.FC = () => {
                 <div className=' h-full gap-4 p-4 items-start max-md:justify-start md:justify-center md:flex md:flex-wrap max-md:w-fit max-md:grid max-md:grid-flow-col max-md:grid-rows-1 mx-auto' >
                     {displayArr?.length != 0 && displayArr.map((item:number)=>(
                         <button onClick={()=>{if(selectedType==0)setSelectedTrait0(item); if(selectedType==1)setSelectedTrait1(item);if(selectedType==2)setSelectedTrait2(item);if(selectedType==3)setSelectedTrait3(item);if(selectedType==4)setSelectedTrait4(item);if(selectedType==5)setSelectedTrait5(item);}} className='flex cursor-pointer items-center justify-center w-40 h-40 bg-white/10 rounded-xl gap-5' >
-                            <Image className='rounded-xl' src={ selectedType==5 && (item==12 || item == 13) ? require(`../../../assets/StickerGenerator/trait_layers/${selectedType}/${item+1}.gif`): require(`../../../assets/StickerGenerator/trait_layers/${selectedType}/${item+1}.png`)} alt={String(item)} />
+                            <img className='rounded-xl' src={ selectedType==5 && (item==12 || item == 13) ? `https://dulk39g224roa.cloudfront.net/trait_layers/${selectedType}/${item+1}.gif`: `https://dulk39g224roa.cloudfront.net/trait_layers/${selectedType}/${item+1}.png`} alt={String(item)} />
                         </button>
                     ))}
                 </div>
@@ -109,37 +106,37 @@ export const StickerGen:React.FC = () => {
             <div id='capture' ref={layeredImageRef} className='max-md:w-[19rem] relative max-md:h-[19rem] overflow-hidden md:w-[32rem] md:h-[32rem] border-[5px] rounded-xl border-dashed border-white/15'>
                 
                     <img 
-                    src={(selectedTrait5==12 || selectedTrait5 == 13) ? require(`../../../assets/StickerGenerator/trait_layers/5/${selectedTrait5+1}.gif`).default.src: require(`../../../assets/StickerGenerator/trait_layers/5/${selectedTrait5+1}.png`).default.src}
+                    src={(selectedTrait5==12 || selectedTrait5 == 13) ? `https://dulk39g224roa.cloudfront.net/trait_layers/5/${selectedTrait5+1}.gif`: `https://dulk39g224roa.cloudfront.net/trait_layers/5/${selectedTrait5+1}.png`}
                     alt="Background" 
                     className='w-full absolute z-0'
                     style={{width:"100%", height:"100%"}}
                     />
                     <img 
-                    src={require(`../../../assets/StickerGenerator/trait_layers/0/1.png`).default.src}
+                    src={`https://dulk39g224roa.cloudfront.net/trait_layers/0/1.png`}
                     alt="Outline" 
                     className='w-screen absolute z-1'
                     style={{width:"100%", height:"100%"}}
                     />
                     <img 
-                    src={require(`../../../assets/StickerGenerator/trait_layers/1/${selectedTrait1+1}.png`).default.src} 
+                    src={`https://dulk39g224roa.cloudfront.net/trait_layers/1/${selectedTrait1+1}.png`} 
                     alt="Head" 
                     className='w-full absolute z-2' 
                     style={{width:"100%", height:"100%"}}
                     />
                     <img 
-                    src={require(`../../../assets/StickerGenerator/trait_layers/2/${selectedTrait2+1}.png`).default.src} 
+                    src={`https://dulk39g224roa.cloudfront.net/trait_layers/2/${selectedTrait2+1}.png`} 
                     alt="Body" 
                     className='w-full absolute z-3' 
                     style={{width:"100%", height:"100%"}}
                     />
                     <img 
-                    src={require(`../../../assets/StickerGenerator/trait_layers/3/${selectedTrait3+1}.png`).default.src} 
+                    src={`https://dulk39g224roa.cloudfront.net/trait_layers/3/${selectedTrait3+1}.png`} 
                     alt="Face" 
                     className='w-full absolute z-4'
                     style={{width:"100%", height:"100%"}} 
                     />
                     <img 
-                    src={require(`../../../assets/StickerGenerator/trait_layers/4/${selectedTrait4+1}.png`).default.src} 
+                    src={`https://dulk39g224roa.cloudfront.net/trait_layers/4/${selectedTrait4+1}.png`} 
                     alt="Chum" 
                     className='w-full absolute z-5' 
                     style={{width:"100%", height:"100%"}}
