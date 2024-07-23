73f9a458c0fd3abfd645a08f929cb306b7d718b5
Risav03 <needleweed03@gmail.com>
2024-07-23 22:05:11 +0530

    go go

--- components/WebsiteLanding/UI/landing.tsx
+++ components/WebsiteLanding/UI/landing.tsx
@@ -9,7 +9,7 @@ export const Landing = () => {
     <div className='flex items-end justify-center bg-[#242326] min-h-screen max-md:object-cover max-md:overflow-hidden max-sm:w-[38rem] max-[640px]:w-[45rem] max-md:w-[50rem] max-lg:w-[70rem] max-xl:w-[90rem] max-2xl:w-[110rem] 2xl:w-[145rem]' >
         <div className='absolute top-[32%] w-full text-center flex items-center flex-col justify-center gap-2'>
           <Image src={chumlogo} alt='logo' className=' max-md:w-[95%] md:w-[60%] mb-5' />
-          <a target='_blank' href='https://opensea.io/collection/chumchumsnft' className='flex gap-2 items-center justify-center md:text-[1.5rem] bg-blue-500 px-4 py-2 rounded-full hover:bg-blue-400 duration-200 hover:-translate-y-1' ><SiOpensea/>OpenSea</a>
+          <a target='_blank' href='https://opensea.io/collection/chumchumsnft' className='flex gap-2 items-center justify-center md:text-[1.5rem] bg-[#e52476] px-4 py-2 rounded-full hover:bg-[#ff4f98] duration-200 hover:-translate-y-1' >Magic Eden</a>
         </div>
         <div className=''>
           <Image src={landingBg} alt='bg1' className='max-sm:w-[38rem] max-[640px]:w-[45rem] max-md:w-[50rem] max-lg:w-[70rem] max-xl:w-[90rem] max-2xl:w-[110rem] 2xl:w-[145rem]'/>
