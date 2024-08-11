11e11c1fe950d02d1039543b9a1298c6e4e3375b
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-08-11 14:22:32 +0530

    meta update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,12 +20,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* just add opengraph image and twitter */}
-        <meta name="twitter:image" content="https://chumchums.xyz/tw-img.png" />
-        <meta name="twitter:image:width" content="1500" />
-        <meta name="twitter:image:height" content="1200" />
-        <meta property="og:image" content="https://chumchums.xyz/og-img.png" />
-        <meta name="og:image:width" content="1500" />
-        <meta name="og:image:height" content="1200" />
+        <meta name="twitter:image" content="https://chumchums.xyz/meta-image.png" />
+        <meta name="twitter:image:width" content="2400" />
+        <meta name="twitter:image:height" content="1260" />
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="og:image" content="https://chumchums.xyz/meta-image.png" />
+        <meta name="og:image:width" content="2400" />
+        <meta name="og:image:height" content="1260" />
       </head>
       <body className={inter.className}>
         <Providers>
