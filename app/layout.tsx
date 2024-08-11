b0ec34f2abb6ea2bce597f2f5fe8767bb6342371
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-08-11 14:04:42 +0530

    opengraph images hardcode

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* just add opengraph image and twitter */}
+        <meta name="twitter:image" content="https://chumchums.xyz/tw-img.png" />
+        <meta name="twitter:image:width" content="1500" />
+        <meta name="twitter:image:height" content="1200" />
+        <meta property="og:image" content="https://chumchums.xyz/og-img.png" />
+        <meta name="og:image:width" content="1500" />
+        <meta name="og:image:height" content="1200" />
+      </head>
       <body className={inter.className}>
         <Providers>
           {children}
